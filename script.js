9e58876737ed5d6faa8968db65310290c5268465
Joakim <joakim.andersson920202@gmail.com>
2022-10-22 08:26:11 +0200

    Create html,css and js files. Plan out project in comments

---
+++ script.js
@@ -0,0 +1,7 @@
+//create add function
+//create subtract function
+//create multiply function
+//create divide function
+
+//Create a new function operate that takes an operator and 2 numbers
+// and then calls one of the above functions on the numbers.
