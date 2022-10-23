70f33a4b214906a2dd9d89e339fba51a0a10818b
Joakim <joakim.andersson920202@gmail.com>
2022-10-23 14:36:31 +0200

    Add function clear and delete

--- script.js
+++ script.js
@@ -1,51 +1,69 @@
 const allButtons = document.querySelectorAll('.buttons');
+const allNumberButtons = document.querySelectorAll('.numberButton');
+const allOperatorButtons = document.querySelectorAll('.operators');
 const screen = document.querySelector('#screen');
+const screenPara = document.querySelector('#screenPara');
+
 const equalButton = document.querySelector('#equal');
-const arrayWithButtons = Array.from(allButtons);
+const clearButton = document.querySelector('#clear');
+const deleteButton = document.querySelector('#delete');
 
-var input = [];
+const arrayWithAllButtons = Array.from(allButtons);
+const arrayWithNumberButtons = Array.from(allNumberButtons);
+const arrayWithOperators = Array.from(allOperatorButtons);
 
-arrayWithButtons.forEach(button => {
+var inputLine1 = [];
+
+arrayWithAllButtons.forEach(button => {
     button.addEventListener('click', userInputs);
 });
-equalButton.removeEventListener('click', userInputs);
-equalButton.addEventListener('click', userInputs);
+equalButton.addEventListener('click', operate);
+clearButton.addEventListener('click', clearAll);
+deleteButton.removeEventListener('click', userInputs);
+deleteButton.addEventListener('click', deleteLastNumber);
 
 
 
 
-
-
-//create add function
 const add = (initialValue, valueToAdd) => initialValue + valueToAdd;
-
-//create subtract function
 const sub = (initialValue, valueToAdd) => initialValue - valueToAdd;
-    
-//create multiply function
 const multiply = (initialValue, valueToAdd) => initialValue * valueToAdd;
-
-//create divide function
 const divide = (initialValue, valueToAdd) => initialValue / valueToAdd;
 
 //Create a new function operate that takes an operator and 2 numbers
 // and then calls one of the above functions on the numbers.
 
-const operate = function (value1, operator, value2){
-    if(operator === "+"){return add(value1, value2)}
-    if(operator === "-"){return sub(value1, value2)}
-    if(operator === "*"){return multiply(value1, value2)}
-    if(operator === "/"){return divide(value1, value2)}
+function operate(value1, operator, value2) {
+
+    if (operator === "+") { return add(value1, value2) }
+    if (operator === "-") { return sub(value1, value2) }
+    if (operator === "*") { return multiply(value1, value2) }
+    if (operator === "/") { return divide(value1, value2) }
 }
 // create a function to display input on calculator screen
-function displayNumbers(){
-    screen.textContent = input;
+function displayNumbers(array) {
+    let removeCommas;
+    removeCommas = array.join("");
+   return screenPara.textContent = removeCommas;
+
+
 }
 
 //function to store inputs from user
 function userInputs() {
-    input.push(this.textContent);
+    if (inputLine1.length >= 20) return;
+    inputLine1.push(this.textContent);
 
-    displayNumbers();
+    displayNumbers(inputLine1);
 
+}
+
+function clearAll() {
+    inputLine1.length = 0;
+    screenPara.textContent = "";
+}
+
+function deleteLastNumber(array) {
+   inputLine1.pop();
+   return displayNumbers(inputLine1);
 }
