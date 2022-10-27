23ad789b11389b2bb6e316d7d5180ffc4859ee20
Joakim <joakim.andersson920202@gmail.com>
2022-10-27 09:09:36 +0200

    Update inputObject to inputArray and update all functions to handle array

--- script.js
+++ script.js
@@ -13,78 +13,92 @@ const arrayWithNumberButtons = Array.from(allNumberButtons);
 const arrayWithOperators = Array.from(allOperatorButtons);
 
 
-var inputLineIndex = 1; 
-var inputObjects = {
+var inputLineIndex = 0; 
+var inputArray = [];
 
-    1 : [],
-}; 
-
-arrayWithAllButtons.forEach(button => {
+arrayWithNumberButtons.forEach(button => {
     button.addEventListener('click', userInputs);
 });
 arrayWithOperators.forEach(button => {
-    button.addEventListener('click', createNewArray);
+    button.addEventListener('click', nextPosInArray);
 });
-equalButton.addEventListener('click', operate);
+// equalButton.addEventListener('click', operate);
 clearButton.addEventListener('click', clearAll);
-deleteButton.removeEventListener('click', userInputs);
 deleteButton.addEventListener('click', deleteLastNumber);
 
 
 
 
-const add = (initialValue, valueToAdd) => initialValue + valueToAdd;
-const sub = (initialValue, valueToAdd) => initialValue - valueToAdd;
-const multiply = (initialValue, valueToAdd) => initialValue * valueToAdd;
-const divide = (initialValue, valueToAdd) => initialValue / valueToAdd;
+// const add = (initialValue, valueToAdd) => initialValue + valueToAdd;
+// const sub = (initialValue, valueToAdd) => initialValue - valueToAdd;
+// const multiply = (initialValue, valueToAdd) => initialValue * valueToAdd;
+// const divide = (initialValue, valueToAdd) => initialValue / valueToAdd;
 
 //Create a new function operate that takes an operator and 2 numbers
 // and then calls one of the above functions on the numbers.
 
-function operate(value1, operator, value2) {
+// function operate(value1, operator, value2) {
 
-    if (operator === "+") { return add(value1, value2) }
-    if (operator === "-") { return sub(value1, value2) }
-    if (operator === "*") { return multiply(value1, value2) }
-    if (operator === "/") { return divide(value1, value2) }
-}
+//     if (operator === "+") { return add(value1, value2) }
+//     if (operator === "-") { return sub(value1, value2) }
+//     if (operator === "*") { return multiply(value1, value2) }
+//     if (operator === "/") { return divide(value1, value2) }
+// }
 // create a function to display input on calculator screen
-function displayNumbers(array) {
-    let removeCommas;
-    removeCommas = array.join("");
-   return screenPara.textContent = removeCommas;
 
+//TODO
+//loop thrue all propertys in object and display.
+const displayNumbers = (array => screenPara.textContent = array.join(""));
 
-}
+function createArrayElementFromInput(buttonValue){
+   
+    if(inputArray[inputLineIndex] === undefined){
+        inputArray.push(buttonValue);
+    }
+    else {
+        const newElementValue = inputArray[inputLineIndex] + buttonValue;
+        inputArray[inputLineIndex] = newElementValue;
+    }
+} 
 
 //function to store inputs from user
-function userInputs(index) {
-    if (inputObjects["1"].length >= 20) return;
-    inputObjects["1"].push(this.textContent);
-
-    displayNumbers(inputObjects["1"]);
-
+function userInputs() {
+    createArrayElementFromInput(this.value);
+    displayNumbers(inputArray);
 }
 
 function clearAll() {
-    for(obj in inputObjects){
-        if(inputObjects.hasOwnProperty(obj)){
-            delete inputObjects[obj];
-        }
-    }
+    inputArray.length = 0;
     inputLineIndex = 0;
     screenPara.textContent = "";
-    createNewArray();
+    nextPosInArray();
 }
 
 function deleteLastNumber() {
-    inputObjects["1"].pop();
-   return displayNumbers(inputObjects["1"]);
+
+    let lastElementValue = [...inputArray[inputLineIndex]];
+    if(lastElementValue.length <= 0 && inputArray.length !== 1){ 
+        inputArray.pop();
+        inputArray.pop();
+        inputLineIndex = inputLineIndex - 2; 
+        displayNumbers(inputArray);
+    }
+    else {
+        lastElementValue.pop();
+    inputArray[inputLineIndex] = lastElementValue.join("");
+    displayNumbers(inputArray);
+    }
+    
+
+    
+    
 }
 
-function createNewArray() {
+function nextPosInArray() {
+    
     inputLineIndex++;
-    const convertToString = inputLineIndex.toString();
-    inputObjects[convertToString] = [];
-    console.log(inputObjects);
-}
+    inputArray.push(this.textContent);
+    displayNumbers(inputArray);
+    inputLineIndex++;
+    console.log(inputArray);
+};
